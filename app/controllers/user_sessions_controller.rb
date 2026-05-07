f0749efc4338c08d804020bcc64fc2b488035f42
Horiuchi <knkncfrfrri@gmail.com>
2026-05-07 11:24:59 +0900

    エラー修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,22 +1,22 @@
 class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
 
-  def new 
+  def new
   end
 
   def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path, notice: 'ログインしました'
+      redirect_to root_path, notice: "ログインしました"
     else
-      flash.now[:alert] = 'ログインに失敗しました'
+      flash.now[:alert] = "ログインに失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other, notice: 'ログアウトしました'
+    redirect_to root_path, status: :see_other, notice: "ログアウトしました"
   end
 end
