b7ff3710aec261724fab390cf8d1c4ea86ca2e56
Horiuchi <knkncfrfrri@gmail.com>
2026-05-08 10:15:40 +0900

    マイページ設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#top"
   resources :users, only: %i[new create]
   resources :boards, only: %i[index]
+  resource :mypage, only: %i[show], controller: "mypages"
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
