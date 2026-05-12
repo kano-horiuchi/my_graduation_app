9ec10636f27d8900a6852292f79cef6aafa17d62
Horiuchi <knkncfrfrri@gmail.com>
2026-05-12 13:39:14 +0900

    外部ストレージ設定

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -4,10 +4,6 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   # include CarrierWave::Vips
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
