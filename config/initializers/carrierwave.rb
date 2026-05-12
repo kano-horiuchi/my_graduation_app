8d4b2a373d5a29fb2e14cdb0156f7eb70a67bedc
Horiuchi <knkncfrfrri@gmail.com>
2026-05-12 13:44:43 +0900

    エラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
+require "carrierwave/storage/fog"
 
-CarrierWave.configure do |config| 
+CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 'sakesearch-app-storage'
+    config.fog_provider = "fog/aws"
+    config.fog_directory = "sakesearch-app-storage"
     config.fog_credentials = {
-      provider: 'aws',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      provider: "aws",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: "ap-northeast-1",
       path_style: true
     }
   else
