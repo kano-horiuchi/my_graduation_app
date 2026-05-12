fdc843212069bb60ceed1653d849b90ab4afc5e2
Horiuchi <knkncfrfrri@gmail.com>
2026-05-12 18:33:54 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,3 @@
-Rails.logger.info "=== CarrierWave Environment Check ==="
-Rails.logger.info "Rails.env: #{Rails.env}"
-Rails.logger.info "Rails.env.production?: #{Rails.env.production?}"
-Rails.logger.info "AWS_ACCESS_KEY_ID: #{ENV['AWS_ACCESS_KEY_ID'].present? ? '設定あり' : '設定なし'}"
-Rails.logger.info "AWS_SECRET_ACCESS_KEY: #{ENV['AWS_SECRET_ACCESS_KEY'].present? ? '設定あり' : '設定なし'}"
-Rails.logger.info "S3_BUCKET: #{ENV['S3_BUCKET'].present? ? ENV['S3_BUCKET'] : '設定なし'}"
-Rails.logger.info "===================================="
-
 require "carrierwave/storage/abstract"
 require "carrierwave/storage/file"
 require "carrierwave/storage/fog"
