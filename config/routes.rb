9484ca4faa55261d7cf6ac8ab34abef7751d7beb
Horiuchi <knkncfrfrri@gmail.com>
2026-05-29 10:29:15 +0900

    管理者画面機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -16,20 +16,14 @@ Rails.application.routes.draw do
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   namespace :admin do
-    get "dashboards/index"
     root "dashboards#index"
+    resources :users, only: [ :index, :show, :edit, :update, :destroy ]
+    resources :boards, only: [ :index, :destroy ]
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
